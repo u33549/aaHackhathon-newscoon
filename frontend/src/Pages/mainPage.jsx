9c96e8a4485a1e147c9569de7fcb4183909f7f3e
u33549 <79751117+u33549@users.noreply.github.com>
2025-10-05 19:18:38 +0300

    Update mainPage.jsx

--- frontend/src/Pages/mainPage.jsx
+++ frontend/src/Pages/mainPage.jsx
@@ -3,21 +3,12 @@ import { Container, Typography, Paper, Box } from '@mui/material';
 
 function MainPage() {
     return (
-        <Container maxWidth="lg" sx={{ mt: 4 }}>
-            <Paper elevation={3} sx={{ p: 4 }}>
-                <Typography variant="h2" component="h1" color="primary" gutterBottom>
-                    Ana Sayfa
-                </Typography>
-                <Typography variant="h6" color="text.primary">
-                    React Router ve Material-UI başarıyla kuruldu!
-                </Typography>
-                <Box sx={{ mt: 2 }}>
-                    <Typography variant="body1" color="text.primary">
-                        Bu proje Material-UI'nin dark teması ile yapılandırılmıştır.
-                    </Typography>
-                </Box>
-            </Paper>
-        </Container>
+        <Box>
+            <Box sx={{width: '100%',height: '10vh',backgroundColor: 'rgba(20,20,20,0.6)'}}>
+
+
+            </Box>
+        </Box>
     );
 }
 
